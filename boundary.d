08285f4bae5e346557e754d5f888af4ac47709aa
rakahori <113431346+rakahori@users.noreply.github.com>
2022-10-09 17:21:14 +0900

    Add files via upload

---
+++ boundary.d
@@ -0,0 +1,11 @@
+3,0,3,1 ! periodic u, right slip w 
+0,0,0,0 ! nonslip,w=0
+1,1,0,0 ! leftright open, dp/dx=0, dp/dz=0
+0,0,0,0 ! top given, other dr/dx=0, dr/dz=0
+
+
+
+      read(12,*) ib1u,ib1w,ib2u,ib2w
+      read(12,*) ib3u,ib3w,ib4u,ib4w
+      read(12,*) ib1p,ib2p,ib3p,ib4p
+      read(12,*) ib1r,ib2r,ib3r,ib4r
---
+++ common.h
@@ -0,0 +1,6 @@
+      integer im,km,nx,nz
+      real dt,time
+      parameter(im=252,km=62)
+      common /cfndi/ nx,nz
+      common /cfndr/ dt,time
+
